45fa7e7df187bbde8f7459c9ba2fcd5fb6856fd8
Brian Ye <brian.ye2021@gmail.com>
2023-03-13 18:51:16 -0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f47e9071-ffb8-4fc1-a220-47f64bbc3b5c';
+export const MODAL_CLOSE_SEC = 2.5;
